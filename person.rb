2458d67395fdd59657546df3a611fb5e43ae20b2
jic <ji_camargo@yahoo.com>
2023-08-01 18:03:35 -0500

    add decorator classes to person.rb

--- person.rb
+++ person.rb
@@ -1,15 +1,29 @@
 # person.rb
+
+#
+# Interface Class Nameable, for decorator design
+#
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
 #
 # Class representing a person in the library application
 #
-class Person
+class Person < Nameable
   attr_accessor :name, :age
   attr_writer :id
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -20,5 +34,46 @@ class Person
 
   def of_age?
     @age.to_i >= 18
+  end
+end
+
+#
+# Base decorator class
+#
+class PersonDecorator < Nameable
+  attr_accessor :person
+
+  def initialize(person)
+    @person = person
+  end
+
+  # The Decorator delegates all work to the wrapped component (person)
+  def correct_name
+    @person.correct_name
+  end
+end
+
+# Concrete Decorators 
+# call the wrapped object and alter its result in some way.
+# 
+# Decorator class: CapitalizeDecorator
+#
+class CapitalizeDecorator < PersonDecorator
+  def correct_name
+    @person.correct_name.capitalize
+  end
+end
+
+# Concrete Decorators 
+# call the wrapped object and alter its result in some way.
+# 
+# Decorator class: TrimmerDecorator
+# Implement a method correct_name that makes sure that the output of 
+# @nameable.correct_name has a maximum of 10 characters. 
+# If it's longer it should trim the word.
+class TrimmerDecorator < PersonDecorator
+  def correct_name
+    name = @person.correct_name
+    name.length > 10 ? name[0..9] : name
   end
 end
